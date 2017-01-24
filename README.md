0ae9321f3045062d0ff3e7434fc39175842216ff
Richard Li <aspirewit@gmail.com>
2017-01-24 18:58:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ionic-scrollable-time-picker
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var del = require('del');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngHtml2Js = require('gulp-ng-html2js');
+var minifyHtml = require('gulp-minify-html');
+var sass = require('gulp-sass');
+var css2js = require('gulp-css2js');
+
+gulp.task('html2js', function () {
+  return gulp.src(['./src/*.html'])
+    .pipe(minifyHtml())
+    .pipe(ngHtml2Js({
+      moduleName: 'ionic-scrollable-time-picker.templates'
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css2js', function () {
+  return gulp.src(['./src/*.scss', './vendor/*/*.css'])
+    .pipe(sass())
+    .pipe(css2js())
+    .pipe(concat('styles.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('make-bundle', ['del', 'html2js', 'css2js'], function () {
+  return gulp.src(['./dist/*', './src/*.js', './vendor/*/*.js'])
+    .pipe(concat('ionic-scrollable-time-picker.bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('del-temp-files', ['make-bundle'], function () {
+  del(['./dist/templates.js', './dist/styles.js']);
+});
+
+gulp.task('del', function () {
+  del(['./dist/*']);
+});
+
+gulp.task('build', ['del-temp-files']);
